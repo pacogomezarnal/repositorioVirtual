e4eaaeab710b5d978e6fb75c425d26b2bf04c757
Paco Gomez <p.gomez@geekshubs.com>
2020-06-23 17:26:36 +0200

    Compra 2

--- cesta.js
+++ cesta.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    let agregarFila = function (producto) {
+        console.log(producto);
+        /*
+        let cuerpo = document.getElementsByTagName('tbody')[0];
+
+        //Crear una fila nueva en la tabla en la que pondremos 5 celdas
+        let nuevaFila = document.createElement('tr');
+        cuerpo.appendChild(nuevaFila);
+        //Una celda con el id de la compra
+        let celdaNombre = document.createElement('td');
+        celdaNombre.textContent = alimento;*/
+    }
+
     botonCheck.addEventListener('click', (event) => {
         var cesta = JSON.parse(data);
-        console.log("Llego");
+        cesta.cesta["detalles"].forEach(producto => {agregarFila(producto)});
+        console.log(cesta);
 
     });
 
